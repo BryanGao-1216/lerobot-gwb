bba36a540bb11665b525d380fca45a5c1fcfe4db
40908 <wenbin991216@163.com>
2026-08-28 00:22:16 +0800

    0826

--- action_token_eval.sh
+++ action_token_eval.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
 
 python action_token_eval/evaluate_action_tokens.py \
-  --policy-path=/data1/gaowenbing/WorkSpace/models/smol_actionmem-final/checkpoints/last/pretrained_model \
+  --policy-path=/data1/gaowenbing/WorkSpace/models/smol_actionmem-final/checkpoints/last/pretrained_model  \
   --effect-tokenizer-checkpoint=/data1/gaowenbing/WorkSpace/MyStudy/effectTokenizer/outputs/effect_vqvae.pt \
   --dataset-root=/data1/gaowenbing/WorkSpace/datasets/OpenX \
   --dataset-repo-id=libero_only \
@@ -13,5 +13,5 @@ python action_token_eval/evaluate_action_tokens.py \
   --num-parallel-calls=8 \
   --num-samples=10 \
   --top-k=5 \
-  --device=cuda \
+  --device=cpu \
   --output-dir=/data1/gaowenbing/WorkSpace/models/smol_actionmem-final/action_token_eval
