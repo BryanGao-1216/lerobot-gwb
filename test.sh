137d2c20096785bc44412ba58fcb86270a54e705
40908 <wenbin991216@163.com>
2026-09-01 18:46:43 +0800

    0901

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=1
 
 lerobot-eval \
   --output_dir="./outputs/eval/libero_smol_actionmem_rlds" \
   --env.gripper_action_convention=oxe \
   --policy.path="/data1/gaowenbing/WorkSpace/models/smolvla-libero-baseline-rlds/checkpoints/last/pretrained_model" \
   --policy.use_peft=false \
-  --env.camera_name_mapping='{"agentview_image":"image","robot0_eye_in_hand_image":"image3"}' \
+  --env.camera_name_mapping='{"agentview_image":"image","robot0_eye_in_hand_image":"image2"}' \
   --env.type=libero \
   --env.task=libero_object,libero_spatial \
   --env.control_mode=relative \
--- train_smolw_lr.sh
+++ train_smolw_lr.sh
@@ -2,17 +2,20 @@
 
 set -euo pipefail
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,2,5
 
 HORIZON="${HORIZON:-20}"
 MEMORY_STRIDE="${MEMORY_STRIDE:-1}"
-BATCH_SIZE="${BATCH_SIZE:-8}"
+BATCH_SIZE="${BATCH_SIZE:-64}"
 OUTPUT_DIR="${OUTPUT_DIR:-/data1/gaowenbing/WorkSpace/models/smolw-libero}"
-TENSORBOARD_ENABLE="${TENSORBOARD_ENABLE:-true}"
-TENSORBOARD_LOG_FREQ="${TENSORBOARD_LOG_FREQ:-10}"
 TENSORBOARD_LOG_DIR="${TENSORBOARD_LOG_DIR:-${OUTPUT_DIR}/tensorboard}"
 
-lerobot-train \
+accelerate launch \
+  --multi_gpu \
+  --num_processes=3 \
+  --num_machines=1 \
+  --main_process_port=25901 \
+  "$(which lerobot-train)" \
   --policy.path="/data1/gaowenbing/WorkSpace/models/smolw-base" \
   --policy.vidtwin_checkpoint_path="/data1/gaowenbing/WorkSpace/models/VidTwin/checkpoints/vidtwin_structure_7_7_8_dynamics_7_8.ckpt" \
   --policy.motion_camera_key="observation.images.image" \
@@ -39,7 +42,7 @@ lerobot-train \
   --policy.scheduler_warmup_steps=5000 \
   --policy.scheduler_decay_steps=100000 \
   --policy.scheduler_decay_lr=5e-6 \
-  --policy.tensorboard_enable="${TENSORBOARD_ENABLE}" \
-  --policy.tensorboard_log_freq="${TENSORBOARD_LOG_FREQ}" \
+  --policy.tensorboard_enable=true \
+  --policy.tensorboard_log_freq=10 \
   --policy.tensorboard_log_dir="${TENSORBOARD_LOG_DIR}" \
   --policy.push_to_hub=false
