2583114b6b9899e8eea06d4ac9d53936e871128c
40908 <wenbin991216@163.com>
2026-09-02 15:38:32 +0800

    0902

---
+++ test_smolw.sh
@@ -0,0 +1,15 @@
+
+export CUDA_VISIBLE_DEVICES=1
+
+lerobot-eval \
+  --output_dir="./outputs/eval/smolw_2w" \
+  --policy.path="/data1/gaowenbing/WorkSpace/models/smolw-libero/checkpoints/last/pretrained_model" \
+  --policy.use_peft=false \
+  --env.camera_name_mapping='{"agentview_image":"image","robot0_eye_in_hand_image":"image2"}' \
+  --env.type=libero \
+  --env.task=libero_object,libero_spatial,libero_goal,libero_10 \
+  --env.control_mode=relative \
+  --env.max_parallel_tasks=4 \
+  --eval.batch_size=10 \
+  --eval.n_episodes=5 \
+  --policy.n_action_steps=20
